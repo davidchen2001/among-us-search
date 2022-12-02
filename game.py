2acd3aa562b468b9df05d1f8270a686bab6a7f98
Dioceus <chuwei.chen2001@gmail.com>
2022-12-02 16:22:23 -0500

    add some agent functions

--- game.py
+++ game.py
@@ -1,2 +1,24 @@
 from environment import Graph, Node, PriorityQueue
+from player import Crewmate, Killer
 
+def initialize_agents():
+    crewmates = []
+
+    for i in range(5):
+        crewmates.append(Crewmate())
+    
+    killer = Killer()
+    return crewmates, killer
+
+
+def pathfinding(input):
+    crewmates, killer = initialize_agents()
+
+    crewmate_queue = PriorityQueue()
+    killer_queue = PriorityQueue()
+
+    
+
+
+
+    
